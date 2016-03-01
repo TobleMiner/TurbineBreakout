1b86c9011cc234a35f385d95245654e6ac6b8218
Tobias Schramm <tobleminer@gmail.com>
2016-03-01 16:41:25 +0100

    Removed some debug code

--- src/de/mvcturbine/breakout/Main.java
+++ src/de/mvcturbine/breakout/Main.java
@@ -8,7 +8,6 @@ import javax.swing.SwingUtilities;
 import de.mvcturbine.breakout.game.Breakout;
 import de.mvcturbine.breakout.ui.render.TestGameRender;
 import de.mvcturbine.breakout.world.WorldBreakout;
-import de.mvcturbine.util.geom.Vec2D;
 
 public class Main extends JFrame implements Runnable
 {
@@ -30,20 +29,6 @@ public class Main extends JFrame implements Runnable
 
 	public static void main(String[] args)
 	{
-		// System.out.println(new Vec2D(1, 1).getAngle() / Math.PI * 180);
-		Vec2D vec1 = new Vec2D(1, 1);
-		Vec2D vec2 = new Vec2D(-1, 0);
-		System.out.println(vec1.getAngle() / Math.PI * 180);
-		System.out.println(vec2.getAngle() / Math.PI * 180);
-		// System.out.println(vec.setAngle(vec.getAngle()).getAngle() / Math.PI
-		// * 180);
-		// System.out.println(new Vec2D(-1, 1).angle() / Math.PI * 180);
-		// System.out.println(new Vec2D(-1, -1).angle() / Math.PI * 180);
-		// System.out.println(new Vec2D(1, -1).angle() / Math.PI * 180);
-		// Line2D line1 = new Line2D(new Loc2D(), new Vec2D(1, 1));
-		// Line2D line2 = new Line2D(new Loc2D(0, 1), new Vec2D(1, -1));
-		// System.out.println(line1.intersection(line2).toString());
-		// System.exit(0);
 		SwingUtilities.invokeLater(new Main());
 	}
 }
