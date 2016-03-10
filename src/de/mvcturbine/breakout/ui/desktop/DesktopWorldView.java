15e3b554b43926172dbf0488005d6eed0139f7fa
Florian <floriansiewers@live.de>
2016-03-10 01:47:42 +0100

    Blocks now use textures

--- src/de/mvcturbine/breakout/ui/desktop/DesktopWorldView.java
+++ src/de/mvcturbine/breakout/ui/desktop/DesktopWorldView.java
@@ -10,7 +10,7 @@ import java.util.Observable;
 
 import javax.swing.JPanel;
 
-import de.mvcturbine.breakout.ui.desktop.render.entity.BlockColorRender;
+import de.mvcturbine.breakout.ui.desktop.render.entity.BlockImageRender;
 import de.mvcturbine.breakout.ui.desktop.render.entity.GenericEntityMeshRender;
 import de.mvcturbine.breakout.world.WorldBreakout;
 import de.mvcturbine.breakout.world.entity.EntityBlock;
@@ -21,45 +21,38 @@ import de.mvcturbine.util.geom.Size2D;
 import de.mvcturbine.world.World;
 import de.mvcturbine.world.entity.Entity;
 
-public class DesktopWorldView extends JPanel implements View
-{
+public class DesktopWorldView extends JPanel implements View {
 	private final RenderRegistry renderRegistry;
 	private final World world;
 
-	public DesktopWorldView(WorldBreakout world)
-	{
+	public DesktopWorldView(WorldBreakout world) {
 		super();
 		this.world = world;
 		this.renderRegistry = new RenderRegistry();
 		this.renderRegistry.registerRender(new GenericEntityMeshRender(), Entity.class);
-		this.renderRegistry.registerRender(new BlockColorRender(), EntityBlock.class);
+		this.renderRegistry.registerRender(new BlockImageRender(), EntityBlock.class);
 	}
 
 	@Override
-	public void update(Observable arg0, Object arg1)
-	{
+	public void update(Observable arg0, Object arg1) {
 		this.repaint();
 	}
 
 	@Override
-	protected void paintComponent(Graphics g)
-	{
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Dimension size = this.world.getSize();
 		int width = getWidth();
 		int height = getHeight();
-		Size2D scale = new Size2D((double) width / size.width,
-				(double) height / size.height);
-		BufferedImage img = new BufferedImage(width, height,
-				BufferedImage.TYPE_4BYTE_ABGR);
+		Size2D scale = new Size2D((double) width / size.width, (double) height / size.height);
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
 		Graphics2D graphics = (Graphics2D) img.getGraphics();
 		graphics.setBackground(Color.WHITE);
 		graphics.clearRect(0, 0, width, height);
-		for(Entity ent : new ArrayList<Entity>(this.world.getAllEntities()))
-		{
-			EntityRender render = (EntityRender) this.renderRegistry
-					.getRender(ent.getClass());
-			if(render != null) render.renderEntity(ent, img, scale);
+		for (Entity ent : new ArrayList<Entity>(this.world.getAllEntities())) {
+			EntityRender render = (EntityRender) this.renderRegistry.getRender(ent.getClass());
+			if (render != null)
+				render.renderEntity(ent, img, scale);
 		}
 		g.drawImage(img, 0, 0, null);
 	}
